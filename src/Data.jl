ed5069bb5d1f22fd32a233c38c1117bbca7cc0b7
Степан Захаров <stepanzh@gmail.com>
2021-05-18 22:12:56 +0300

    Add caching in Data submodule. Closes #7.
    CachedDatabase is responsible object. It stores necessary information for constructor of a type T.
    Actual object CachedDatabase.value is initiated at first call value(x::CachedDatabase).
    
    There some problems around @code_warntype Data.value(Data.CachedMartinez) and other CachedDatabases.
    I think it is because parametric type of a database is known only after initiating.
    
    But... @allocated Data.martinez() gives 0 after second call.
    
    Also, update version of package.

--- src/Data.jl
+++ src/Data.jl
@@ -13,28 +13,49 @@ using ..CubicEoSDatabase
 
 export martinez, brusilovsky_comp, brusilovsky_mix
 
-"Root path of database source files."
-root = joinpath(abspath(@__DIR__), "../data")
+"Cache of a database `::T`. Stores arguments needed to initialize `T`."
+mutable struct CachedDatabase{T,A,KW}
+    value::Union{Nothing,T}
+    calculated::Bool
+    args::A     # `T(args...; kwargs...)` constructor arguments
+    kwargs::KW  #
+    function CachedDatabase(::Type{T}, args, kwargs) where {T}
+        new{T,typeof(args),typeof(kwargs)}(nothing, false, args, kwargs)
+    end
+end
 
-"I Martinez gas properties."
-martinez() = ComponentDatabase(
-    joinpath(root, "phys/comp/martinez.csv"),
-    reference="Website (loaded in 2017): http://imartinez.etsiae.upm.es/~isidoro/dat1/eGAS.pdf",
-    delim=','
+value(x::CachedDatabase{T,A,KW}) where {T,A,KW} = begin
+    if !x.calculated
+        x.value = T(x.args...; x.kwargs...)
+        x.calculated = true
+    end
+    return x.value::T
+end
+
+
+"Root path of database source files."
+const root = joinpath(abspath(@__DIR__), "../data")
+
+CachedMartinez = CachedDatabase(ComponentDatabase,
+    (joinpath(root, "phys/comp/martinez.csv"),),
+    (reference="Website (loaded in 2017): http://imartinez.etsiae.upm.es/~isidoro/dat1/eGAS.pdf", delim=',')
 )
+"I Martinez gas properties."
+martinez() = value(CachedMartinez)
 
 brusilovsky_book = "A I Brusilovskii. Fazovie prevrasheniya pri razrabotke mestorozhdeniy nefti i gaza (Phase transitions during mining of petroleum and gas). 2002. Moscow, Graal’. ISBN: 5-94688-031-4. (in Russian)"
+CachedBrusilovskyComp = CachedDatabase(ComponentDatabase,
+    (joinpath(root, "eos/comp/brusilovsky.csv"),),
+    (reference=brusilovsky_book, delim=',')
+)
 "A I Brusilovskii equation of state parameters for pure components."
-brusilovsky_comp() = ComponentDatabase(
-    joinpath(root, "eos/comp/brusilovsky.csv"),
-    reference=brusilovsky_book,
-    delim=','
+brusilovsky_comp() = value(CachedBrusilovskyComp)
+
+CachedBrusilovskyMixture = CachedDatabase(MixtureDatabase,
+    (joinpath(root, "eos/mix/brusilovsky.csv"),),
+    (reference=brusilovsky_book, delim=',')
 )
 "A I Brusilovskii equation of state binary parameters for mixtures."
-brusilovsky_mix() = MixtureDatabase(
-    joinpath(root, "eos/mix/brusilovsky.csv"),
-    reference=brusilovsky_book,
-    delim=','
-)
+brusilovsky_mix() = value(CachedBrusilovskyMixture)
 
 end # module
